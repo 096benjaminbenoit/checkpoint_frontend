a136ad175dcab28e41abdb60bec176763f0fe98d
096benjaminbenoit <096benjaminbenoit@gmail.com>
2024-07-08 11:16:13 +0200

    fetch a country with code

--- frontend/src/pages/country/[countryId].tsx
+++ frontend/src/pages/country/[countryId].tsx
@@ -1,3 +1,5 @@
+import CountryDetailsCard from "@/components/CountryDetailsCard";
+import { useCountryByCodeQuery } from "@/graphql/generated/schema";
 import DefaultLayout from "@/layouts/defaultLayout";
 import { useRouter } from "next/router";
 
@@ -5,9 +7,27 @@ export default function CountryDetailsPage() {
   const router = useRouter();
   const { countryId } = router.query;
 
+  const { data, loading } = useCountryByCodeQuery({
+    variables: { code: countryId as string },
+  });
+
+  if (loading) return "Chargement...";
+
+  if (!data) return "Aucun pays n'a pu être trouvé";
+
+  const country = data.country;
+
   return (
     <DefaultLayout title="Country details">
-      <div>Country {countryId}</div>
+      <div className="flex justify-center pt-20">
+        <CountryDetailsCard
+          code={countryId as string}
+          // @ts-ignore
+          continent={country.continent}
+          emoji={country.emoji}
+          name={country.name}
+        />
+      </div>
     </DefaultLayout>
   );
 }
