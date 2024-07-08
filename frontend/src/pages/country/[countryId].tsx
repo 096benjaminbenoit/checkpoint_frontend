d553976835f367f3dd6569106164131a05adf11b
096benjaminbenoit <096benjaminbenoit@gmail.com>
2024-07-08 10:14:29 +0200

    add layout on pages

--- frontend/src/pages/country/[countryId].tsx
+++ frontend/src/pages/country/[countryId].tsx
@@ -1,8 +1,13 @@
+import DefaultLayout from "@/layouts/defaultLayout";
 import { useRouter } from "next/router";
 
 export default function CountryDetailsPage() {
   const router = useRouter();
   const { countryId } = router.query;
 
-  return <div>Country {countryId}</div>;
+  return (
+    <DefaultLayout title="Country details">
+      <div>Country {countryId}</div>
+    </DefaultLayout>
+  );
 }
--- frontend/src/pages/new-country/index.tsx
+++ frontend/src/pages/new-country/index.tsx
@@ -1,5 +1,10 @@
+import DefaultLayout from "@/layouts/defaultLayout";
 import React from "react";
 
 export default function NewCountryPage() {
-  return <div>Create a new country</div>;
+  return (
+    <DefaultLayout title="New country">
+      <div>Create a new country</div>
+    </DefaultLayout>
+  );
 }
