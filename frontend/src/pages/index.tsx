2e4fd54abaa99581511059ce37ad10064b35effa
096benjaminbenoit <096benjaminbenoit@gmail.com>
2024-07-08 11:14:45 +0200

    fetch all countries on homepage

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,31 @@
+import CountryCard from "@/components/CountryCard";
+import { useCountriesQuery } from "@/graphql/generated/schema";
 import DefaultLayout from "@/layouts/defaultLayout";
 
 export default function Home() {
+  const { data, loading } = useCountriesQuery();
+
+  if (loading) return "Chargement...";
+
+  const countries = data?.countries || [];
+
+  console.log(countries);
+
   return (
     <DefaultLayout title="Home">
-      <h1>Homepage with all countries</h1>
+      <div className="flex flex-wrap gap-6 justify-center">
+        {countries.map((country) => (
+          <CountryCard
+            countryId={country.id}
+            emoji={country.emoji}
+            // @ts-ignore
+            continent={country.continent}
+            code={country.code}
+            name={country.name}
+            key={country.id}
+          />
+        ))}
+      </div>
     </DefaultLayout>
   );
 }
